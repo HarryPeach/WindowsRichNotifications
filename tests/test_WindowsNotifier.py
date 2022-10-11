c27939f14e653c3e6b9d4836f80a89ceafee01e2
Harry Peach <harrypeach1999@gmail.com>
2022-10-11 07:33:54 +0100

    Add test for non-blocking notification

--- tests/test_WindowsNotifier.py
+++ tests/test_WindowsNotifier.py
@@ -1,4 +1,5 @@
 """Unit tests for the WindowsNotifier class"""
+from typing import Callable
 from winrichnotify import WindowsNotifier
 from expects import expect, raise_error, be_within
 import time
@@ -12,8 +13,23 @@ def notifier() -> WindowsNotifier:
     return WindowsNotifier()
 
 
+def _validate_time_taken(notif_duration: int, margin: float, measured_func: Callable):
+    """Checks that a notification has taken a specific amount of time to show
+
+    Args:
+        notif_duration (int): The duration of the notification
+        margin (float): The margin of error
+        measured_func (Callable): The function to measure the time of
+    """
+    start_time: float = time.time()
+    measured_func()
+    observed_duration = time.time() - start_time
+    expect(observed_duration).to(
+        be_within(notif_duration - margin, notif_duration + margin))
+
+
 def test_create_notification_without_body(notifier: WindowsNotifier) -> None:
-    """Tests that a notification cannot be created without a body,
+    """Test that a notification cannot be created without a body,
     and instead will throw an error.
 
     Args:
@@ -28,14 +44,19 @@ def test_notification_delay(notifier: WindowsNotifier) -> None:
     Args:
         notifier (WindowsNotifier): The instance of WindowsNotifier to use
     """
-    margin: float = 0.5
-
-    def test_time(notif_duration: int):
-        start_time: float = time.time()
-        notifier.notify("<Example Body>", duration=notif_duration)
-        observed_duration = time.time() - start_time
-        expect(observed_duration).to(
-            be_within(notif_duration - margin, notif_duration + margin))
+    margin: float = 0.8
 
     for i in range(0, 5):
-        test_time(i)
+        _validate_time_taken(i, margin, lambda: notifier.notify(
+            "<Example Body>", duration=i))
+
+
+def test_non_blocking_notifications(notifier: WindowsNotifier) -> None:
+    """Test that a notification does not block program execution
+
+    Args:
+        notifier (WindowsNotifier): The instance of WindowsNotifier to use
+    """
+    margin: float = 0.2
+    _validate_time_taken(0, margin, lambda: notifier.notify(
+        "<Example Body>", duration=0, threaded=True))
